70021c4afd98dab33d24f2f43e135baedd857b9b
Zhijie Cui <Zhijie Cui@169.254.26.64>
2016-11-22 19:29:02 -0500

    DoublyLinkedList

---
+++ bobsUtility/src/bobsUtility/DoublyLinkedList.java
@@ -0,0 +1,22 @@
+package bobsUtility;
+
+public class DoublyLinkedList<E> {
+
+	class Node<E>{
+		public E data;
+		public Node<E> nextNode;
+		public Node<E> lastNode;
+		
+		public Node(E newData,Node<E> nextNode, Node<E> lastNode){
+			
+		}
+		
+	}
+	
+	private Node<E> head;
+	
+	public void push(E newData)
+	{
+		
+	}
+}
