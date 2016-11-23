a6919f6e633f803b6978eca55e1c575651551400
Zhijie Cui <Zhijie Cui@169.254.26.64>
2016-11-22 19:55:17 -0500

    Complete implementation of DoublyLinkedList

--- bobsUtility/src/bobsUtility/DoublyLinkedList.java
+++ bobsUtility/src/bobsUtility/DoublyLinkedList.java
@@ -8,7 +8,9 @@ public class DoublyLinkedList<E> {
 		public Node<E> lastNode;
 		
 		public Node(E newData,Node<E> nextNode, Node<E> lastNode){
-			
+			this.data = newData;
+			this.nextNode = nextNode;
+			this.lastNode = lastNode;
 		}
 		
 	}
@@ -17,6 +19,49 @@ public class DoublyLinkedList<E> {
 	
 	public void push(E newData)
 	{
-		
+		Node<E> newNode = new Node<E>(newData,head,null);
+		if(head!=null)
+		{
+			head.lastNode = newNode;
+		}
+		newNode.lastNode = null;
+		newNode.nextNode = head;
+		head = newNode;
+	}
+	
+	public E pop()
+	{
+		Node<E> endNode = head;
+		while(endNode.nextNode != null)
+		{
+			endNode = endNode.nextNode;
+		}
+		E popData = endNode.data;
+		endNode.lastNode.nextNode = null;
+		return popData;
+	}
+	
+	public String toString()
+	{
+		String str  = "";
+		Node<E> endNode = head;
+		while(endNode!=null)
+		{
+			if(endNode.nextNode!=null && endNode.lastNode!=null)
+			{
+				str += endNode.lastNode.data.toString() + "->" + endNode.data.toString() + "->" + endNode.nextNode.data.toString();
+			}
+			else if(endNode.lastNode == null)
+			{
+				str += "Null" + "->" + endNode.data.toString() + "->" + endNode.nextNode.data.toString();
+			}
+			else
+			{
+				str += endNode.lastNode.data.toString() + "->" + endNode.data.toString() + "->" + "Null";
+			}
+			str+="\n";
+			endNode = endNode.nextNode;
+		}
+		return str;
 	}
 }
